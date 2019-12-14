338461e86542cccbe497f56b839a81adb8501b16
Domovoy <afss_rf@list.ru>
2019-12-14 11:58:22 +1000

    3

---
+++ src/ru/khalus/chap01/task03/Main.java
@@ -0,0 +1,10 @@
+package ru.khalus.chap01.task03;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 45;
+        System.out.println(x);
+        int y = 78;
+
+    }
+}
